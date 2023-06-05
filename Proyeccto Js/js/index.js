08c4f79f4699594d8a5245689f4e8c5351a2ebac
Agustina Acuna <agusbe90@gmail.com>
2023-06-05 15:28:28 +0200

    Agregando de 0

---
+++ Proyeccto Js/js/index.js
@@ -0,0 +1,69 @@
+console.log('hola agus')
+
+//palabras reservadas- sintaxis
+// var - const - break - instanceof- typeof - if -
+
+
+
+//variables y valores - caja Tipos de datos (string)
+//var nombre = 'Agustina'
+//let apelido = 'Acuna'
+//const CURSO = 'JavaScrip'
+//const curso = 'No es java'
+
+//Declaracion
+//let apellido
+//var nombre
+//const curso
+
+//var nombre_apellido
+//var nombreApellido //camel case
+
+
+
+//var anio 
+
+//asignacion
+//const edad = 30
+//inicializando variables
+//const edad1= 30
+
+// no usar variables creadas con var
+
+//let numero = 1
+//let numero2 = 2
+//const NUMERO3 = 3
+
+///let resultado = numero + numero2 //es 1+2
+//let resultadoResta = numero - numero2 // 1-2
+//let multiplicacion = numero * NUMERO3
+
+//operaciones basicas con string
+
+//let texto1 = "Agus"
+//let texto2 = "Acuna"
+//const texto3 = ''
+
+///let resultadoSuma = texto1 + texto2
+//let resultadoSuma1 = texto1 + 1
+
+
+//console.log(resultadoSuma1)
+
+//let nombreIngresadoPrompt = prompt('Ingresar nombre, apura')
+//console.log(nombreIngresadoPrompt)
+
+
+//alert
+//alert("Bienvenidos a esta página")
+
+//ejemplo
+
+let entrada = prompt('Ingrese una nombre: ')
+console.log(typeof entrada)
+let salida = 'Bienvenid@  '+ entrada
+console.log(salida)
+alert(salida)
+
+
+
