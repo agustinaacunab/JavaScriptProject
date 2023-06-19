74e896bf5d9b1daa6f23559c4b84d911f82f57ee
Agustina Acuna <agusbe90@gmail.com>
2023-06-19 15:05:59 +0200

    Agregando Js

--- Proyeccto Js/js/index.js
+++ Proyeccto Js/js/index.js
@@ -1,69 +1,87 @@
-console.log('hola agus')
+//let entrada = ( prompt('Ingrese un Nombre: ') )
+//console.log(typeof entrada)
 
-//palabras reservadas- sintaxis
-// var - const - break - instanceof- typeof - if -
+//let salida  = entrada 
 
+//console.log(salida)
+ //alert('Bienvenido ' + entrada)
 
+ for (let i = 1; i <= 5; i++) {
+    let ingresarNombre = prompt('ingresar nombre');
+    alert('turno nº ' + i + 'nombre : ' + ingresarNombre);
+  }
+  
+for (let i = 1; i <= 5; i++) {
+    if (i == 5){
+      break;
+    }
+    alert ("Quedan "+ i + " lugares");
+   }
+ var numero = parseInt(prompt('Ingrese Presupuesto a gastar'));
+ if(numero >120000){
+    alert('Con este presupuesto cuenta con diversos tipos de opciones para crear el viaje ideal');
+ } else {
+    alert('Disponemos de ofertas limitadas para elegir');
+ }
 
-//variables y valores - caja Tipos de datos (string)
-//var nombre = 'Agustina'
-//let apelido = 'Acuna'
-//const CURSO = 'JavaScrip'
-//const curso = 'No es java'
+ var texto = prompt("Que tipo de Viaje desea?");
 
-//Declaracion
-//let apellido
-//var nombre
-//const curso
+ if(texto=='Eurotrip'){
+    alert('Disponemos de salidas los dias Domingos, Miercoles y viernes');
+ }else{
+    alert('Disculpe, no disponemos de la opcion que desea en estos momentos');
+ }
 
-//var nombre_apellido
-//var nombreApellido //camel case
-
-
-
-//var anio 
-
-//asignacion
-//const edad = 30
-//inicializando variables
-//const edad1= 30
-
-// no usar variables creadas con var
-
-//let numero = 1
-//let numero2 = 2
-//const NUMERO3 = 3
-
-///let resultado = numero + numero2 //es 1+2
-//let resultadoResta = numero - numero2 // 1-2
-//let multiplicacion = numero * NUMERO3
-
-//operaciones basicas con string
-
-//let texto1 = "Agus"
-//let texto2 = "Acuna"
-//const texto3 = ''
-
-///let resultadoSuma = texto1 + texto2
-//let resultadoSuma1 = texto1 + 1
+ var texto = prompt ("Que día deseas viajar?")
+if (texto == 'Domingo') {
+    alert('Esta opción Tiene un Importe total de 1.500 Euros+IVA')
+}
+else if (texto == 'Miercoles') {
+    alert('Enhorabuena! Hemos conseguido el mejor paquete por solo 1.000 Euros+IVA')
+}
+else if (texto == 'Viernes') {
+    alert('Este paquete tiene un importe total de 1.200 Euros+IVA')
 
 
-//console.log(resultadoSuma1)
+}
 
-//let nombreIngresadoPrompt = prompt('Ingresar nombre, apura')
-//console.log(nombreIngresadoPrompt)
+function calculadora(primerNumero, segundoNumero, operacion) {
+    switch (operacion) {
+        case "+":
+            return primerNumero + segundoNumero;
+            break;
+        case "-":
+            return primerNumero - segundoNumero;
+            break;
+        case "*":
+            return primerNumero * segundoNumero;
+            break;
+        case "/":
+            return primerNumero / segundoNumero;
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+alert("importe total seria de "+(calculadora (1000, 0.5, "*")));
 
 
-//alert
-//alert("Bienvenidos a esta página")
+//let entrada = prompt ("ingrese destino");
+//while (entrada != "ESC"){
+  //switch (entrada){
+ // case "EUROPA":
+  //  alert ("Tenemos variedad de destinos en EUROPA");
+   // break;
+   // case "LATINOAMERICA":
+   //   alert("Contamos con varios paquetes en LATINOAMERICA");
+    //  break;
+   //   case "CRUCEROS":
+    //  alert("Tenemos salidas de CRUCEROS los días jueves");
+    //  break;
+    //  default: 
+    //  alert("Contamos con diversos paquetes adaptados a tu presupuesto")
+    //  break;
+//}
 
-//ejemplo
-
-let entrada = prompt('Ingrese una nombre: ')
-console.log(typeof entrada)
-let salida = 'Bienvenid@  '+ entrada
-console.log(salida)
-alert(salida)
-
-
-
+//}
