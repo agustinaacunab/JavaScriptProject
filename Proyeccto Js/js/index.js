c0a2118c90a52b46dff7ed96890b0572f042cd0e
Agustina Acuna <agusbe90@gmail.com>
2023-07-20 00:46:43 +0200

    3ra preEntrega

--- Proyeccto Js/js/index.js
+++ Proyeccto Js/js/index.js
@@ -1,200 +1,154 @@
-//let entrada = ( prompt('Ingrese un Nombre: ') )
-//console.log(typeof entrada)
+//Entrega3
 
-//let salida  = entrada 
+console.dir(document);
+console.dir(document.head);
+console.dir(document.body);
 
-//console.log(salida)
- //alert('Bienvenido ' + entrada)
+//Codigo Js ID app getElementsbyID
+let div = document.getElementById("app");
+let parrafo = document.getElementById("parrafo1");
+console.log(div.innerHTML);
+console.log(parrafo.innerHTML);
 
- //for (let i = 1; i <= 5; i++) {
-  //  let ingresarNombre = prompt('ingresar nombre');
-   // alert('turno nº ' + i + 'nombre : ' + ingresarNombre);
- // }
-  
-//for (let i = 1; i <= 5; i++) {
-   // if (i == 5){
-    //  break;
-   // }
-   // alert ("Quedan "+ i + " lugares");
-  // }
- //var numero = parseInt(prompt('Ingrese Presupuesto a gastar'));
- //if(numero >120000){
-  //  alert('Con este presupuesto cuenta con diversos tipos de opciones para crear el viaje ideal');
- //} else {
-  //  alert('Disponemos de ofertas limitadas para elegir');
- //}
+//Codigo JS Class paises getElementsbyclassname
 
- //var texto = prompt("Que tipo de Viaje desea?");
+//let paises = document.getElementsByClassName("paises");
 
- //if(texto=='Eurotrip'){
-   // alert('Disponemos de salidas los dias Domingos, Miercoles y viernes');
- //}else{
-   // alert('Disculpe, no disponemos de la opcion que desea en estos momentos');
- //}
+//console.log(paises[1].innerHTML);
+//console.log(paises[2].innerHTML);
 
- //var texto = prompt ("Que día deseas viajar?")
-//if (texto == 'Domingo') {
-    //alert('Esta opción Tiene un Importe total de 1.500 Euros+IVA')
-//}
-//else if (texto == 'Miercoles') {
-   // alert('Enhorabuena! Hemos conseguido el mejor paquete por solo 1.000 Euros+IVA')
-//}
-//else if (texto == 'Viernes') {
-   // alert('Este paquete tiene un importe total de 1.200 Euros+IVA')
+//Codigo JS getelementsbytagname
+
+let paises = document.getElementsByTagName("div");
+console.log(paises[0].innerHTML);
+console.log(paises[1].innerHTML);
 
 
-//}
+let pais = document.getElementsByClassName("paises");
 
-//function calculadora(primerNumero, segundoNumero, operacion) {
- //   switch (operacion) {
-      //  case "+":
-      //      return primerNumero + segundoNumero;
-       //     break;
-       // case "-":
-         //   return primerNumero - segundoNumero;
-         //   break;
-        //case "*":
-         //   return primerNumero * segundoNumero;
-          //  break;
-      //  case "/":
-          //  return primerNumero / segundoNumero;
-          //  break;
-       // default:
-          //  return 0;
-          //  break;
-   // }
-//}
-//alert("importe total con el IVA final seria de "+(calculadora (1000, 5, "*")));
-
-
-//let entrada = prompt ("ingrese destino");
-//while (entrada != "ESC"){
-  //switch (entrada){
- // case "EUROPA":
-  //  alert ("Tenemos variedad de destinos en EUROPA");
-   // break;
-   // case "LATINOAMERICA":
-   //   alert("Contamos con varios paquetes en LATINOAMERICA");
-    //  break;
-   //   case "CRUCEROS":
-    //  alert("Tenemos salidas de CRUCEROS los días jueves");
-    //  break;
-    //  default: 
-    //  alert("Contamos con diversos paquetes adaptados a tu presupuesto")
-    //  break;
-//}
-
-//}
-//                                              Pre Entrga N2                                     ///
-function saludar (){
-    console.log ("Bienvenido a Deviajes.com");
-}
-  saludar();
-
-  function solicitarDestino (){
-    let destinoIngresado = prompt("Ingrese destino")
-    alert ("El destino Seleccionado es " + destinoIngresado)
-  }
-
-  solicitarDestino ();
-
-// Calculando Cuotas
-function dividir(primerNumero, segundoNumero) {
-    var resultado = primerNumero / segundoNumero;
-    return resultado; 
+for (const pais of paises) {
+ console.log(pais.innerHTML);
 }
 
-function mostrar(mensaje) {
-    console.log(mensaje);
+// Innet text- Nos permite modificar su nodo de texto. Acceder y/o modificar el contenido textual
+
+let titulo = document.getElementById("titulo")
+console.log (titulo.innerText)
+
+titulo.innerText = "Bienvenidos a Deviajes.com"
+
+//Innet HTML
+
+let paquete = document.getElementById("opciones")
+paquete.innerHTML = "<h2>Bienvenido al area VIP!!!</h2><p>Lorem ipsum</p>"
+
+//Class name
+
+let ofertas = document.getElementById("lista")
+ ofertas.innerHTML = "<h2>Buenas buenas ! </h2>"
+
+ ofertas.className = "container row"
+
+ //Creacion de elementos
+
+ let parrafo3 = document.createElement("p");
+ parrafo3.innerHTML = "<h2/>Benvenutti al area de descuentos</h2>";
+ document.body.append(parrafo3);
+
+//eliminar elementos
+
+let parrafo1 = document.getElementById("parrafo1");
+parrafo.remove();
+
+//let paises = document.getElementsByClassName("paises");
+//paises [0] . remove()
+
+//Obtener datos de Inputs
+
+document.getElementById("Country").value = "Argentina";
+document.getElementById("Price").value = "1500";
+
+//Creando Opciones desde un Array
+
+let padre = document.getElementById("lista");
+let destinosArray = ["Italia", "Francia", "Etiopia", "Egipto", "España"];
+for (const destino of destinosArray) {
+    let li = document.createElement ("li");
+    li.innerHTML = destino
+    padre.appendChild(li);
 }
 
-var resultadoDivison = dividir(1350, 18);
-mostrar(resultadoDivison); 
-
-//Calculando IVA paquetes y descuentos
-const suma = (a,b) => a + b
-const resta = (a,b) => a - b
-const iva = x => x * 0.21
-let precioPaquete = 1000
-let descuento = 50 
-let precioDescuento = resta(suma(precioPaquete, iva(precioPaquete)), descuento)
-console.log (precioDescuento)
-
-//objetos
-
-let paquete = "Africa Salvaje";
-let precio = 1200;
-let salidas = "vuelos desde Barcelona y Madrid";
- const Ofertas = {paquete: "Africa Salvaje", precio: "1200", salidas: "vuelos desde Barcelona y Madrid"
-}
-console.log (Ofertas.paquete)
-console.log (Ofertas.precio)
-console.log (Ofertas.salidas)
-
-function viajesDePromo (destino, precio, salidas){
-    this.destino = destino;
-    this.precio = precio;
-    this.salidas = salidas;
-}
-const paquete1 = new viajesDePromo ("Tailandia sorprendente", "750", "Vuelos desde Barcelona y Madrid");
-const paquete2 = new viajesDePromo ("Italia Paradisso", "1500", "vuelos desde Barcelona");
-console.log (paquete2);
-
-const paquete3 ={ pais: "Africa Salvaje", CuotasDisponibles :"12", precioFinal: "1200"};
-console.log ("pais" in paquete3);
-console.log ("salidas" in paquete3);
-for(const propiedad in paquete3){
-    console.log(paquete3 [propiedad]);
-}
-//  Clase producto//
-class producto {
-    constructor (destino, precio){
-        this.destino = destino.toUpperCase();
-        this.precio = parseFloat(precio);
-        this.disponibilidad = false;
-
-    }
-    sumaIVA(){
-        this.precio = this.precio * 1.21;
-
-    }
-    venta (){
-        this.disponibilidad = true;
-
-    }
-}
-
-const destino1 = new producto ("Tailandia Sorprendente", "800");
-const destino2 = new producto ("Italia Paradisso", "1200");
-destino1.sumaIVA();
-destino2.sumaIVA();
-destino1.venta();
-
-
-/// Arrays //
-
-
-const listaDestinos = [];
-let cantidad = 6;
-do{
-    let entrada = prompt ("Ingrese destino");
-    listaDestinos.push(entrada.toUpperCase());
-    console.log(listaDestinos.length);
-
-}while (listaDestinos.length != cantidad)
-
-const nuevoDestino = listaDestinos.concat (["Egipto"]);
-alert(nuevoDestino.join("/n"));
-
-//7
-
-const cruceros = [
-    { nombre: 'Italia Paradisso', precio: 1500 },
-    { nombre: 'Sudeste Asiatico', precio: 2200 },
+//creacion de elementos desde objetos
+const promos = [
+    {id:1, 
+    nombre: "italia Paradisso", 
+    precio: 400, categoria: "Europa", 
+    imgUrl:'https://cdn0.bodas.net/article/3813/3_2/1280/jpg/63183-1.webp'},
+    {id:2, 
+    nombre: "Tailandia Sorprendente", 
+    precio:750, categoria: "Asia", 
+    imgUrl:'https://www.eltiempo.com/files/article_main_1200/uploads/2022/05/24/628ce8c59cf62.jpeg'},
+    {id:3, 
+    nombre: "China Milenaria", 
+    precio: 1000, 
+    categoria: "Asia", 
+    imgUrl:'https://www.sprachcaffe.com/fileadmin/_processed_/f/6/csm_China__shanghai_yuyuan_garden__web-2048__e953ddf7e2.jpg'},
+    {id:4, 
+    nombre: "Africa premium", 
+    precio:1200, 
+    categoria:"Africa", 
+    imgUrl:'https://wp.growproexperience.com/wp-content/uploads/2022/06/Safaris-en-Suda%CC%81frica.jpg'}
 ];
+const listadoPromos = document.getElementById('listadoPromos')
+const divRow = document.createElement('div')
+divRow.classList.add('row', 'w-100')
+listadoPromos.appendChild(divRow)
+let contenedor = document.createElement('div')//div row
 
-const resultado = cruceros.find((el) => el.nombre === "Italia Paradisso");
-const resultado2 = cruceros.find((el) => el.nombre === "Sudeste Asiatico");
 
-console.log(resultado); 
-console.log(resultado2); 
+
+for (const promo of promos){
+    const divCard = document.createElement('div')
+    divCard.classList.add('card', 'col-4')
+    divCard.innerHTML = `<div class='card-body'>
+    <img src='${promo.imgUrl} 'class='card-img-top'>
+    <h3>Destino: ${promo.nombre} </h3>
+    <h3>categoria: ${promo.categoria} </h3>
+    <p>Precio: ${promo.precio}</p>
+    <div class='card-footer'>
+    <button class='btn btn-outline-dark id='detalle'>Agregar al carrito</button>
+    </div>
+    </div>`
+    divRow.appendChild(divCard)
+}
+listadoPromos.appendChild(divRow)
+
+//Query selector
+//permite seleccionar nodos con la misma sintaxis que utilizamos en los selectores de CSS
+
+let parrafo2 = document.querySelector("#prueba p")
+let prueba = document.querySelector("#prueba")
+let radioChecked = document.querySelector(".radio:checked")
+//let input =document.querySelectorAll('input')
+//console.log(input)
+
+//Clase 10 -
+//eventos del mouse
+let boton = document.getElementById("btnMain");
+boton.onclick = () => { console.log("click"); };
+boton.onmousemove = () => { console.log("move"); };
+
+//formulario
+
+let miFormularioB = document.getElementById('formulario')
+miFormularioB.onsubmit = (evt)=>{
+    evt.preventDeFault()
+    let form = evt.target
+
+    console.log(form.children[0].value)
+    console.log(form.children[1].value)
+    console.log(form.children[2].value)
+    console.log(form.children[3].value)
+    console.log(form.children[4].value)
+    console.log(form.children[5].value)
+}
